dc8c5471e8103a0e623fb52abb83a8e8de50cd9e
irsanrh <irsanrh275@gmail.com>
2018-11-23 14:38:32 +0700

    Menambahkan code 2

--- week6/[Exercises 6 (Auto)] Palindrome Angka.js
+++ week6/[Exercises 6 (Auto)] Palindrome Angka.js
@@ -1,11 +1,12 @@
 function angkaPalindrome(num) {
   // you can only write your code here!
+  // Function akan me-return angka selanjutnya yang mengandung nilai angka palindrome.
     var angka = num + 1;   
     var reverse = '';   
     while(true) { 
-      var i = String(angka);
+      var i = String(angka); // jika angka adalah 27, maka function akan me-return nilai 33 karena angka 33 adalah angka palindrom.
     reverse = i.split('').reverse().join('');
-    if(angka === Number(reverse))
+    if(angka === Number(reverse)) // jika angka adalah 8, walaupun dia sudah palindrome, harus mencari angka selanjutnya yang palindrome, yaitu 9.
     {return angka;}
     angka++;   
     } 
