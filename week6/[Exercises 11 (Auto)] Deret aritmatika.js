1f770bafa441ac887e1b1d080684fe7186b24eae
irsanrh <irsanrh275@gmail.com>
2018-11-24 11:07:57 +0700

    Menambahkan code7

--- week6/[Exercises 11 (Auto)] Deret aritmatika.js
+++ week6/[Exercises 11 (Auto)] Deret aritmatika.js
@@ -1,8 +1,10 @@
 function tentukanDeretAritmatika(arr) {
   // you can only write your code here!
+  //tentukanDeretAritmatika(arr) yang menerima satu parameter berupa array yang terdiri dari angka.
+  //Function tersebut akan mengembalikan true jika array dari parameter tersebut merupakan deret aritmatika. 
   var deret = arr[1] - arr[0]
   var arit =0;
-  for (var i=0; i < arr.length; i++){
+  for (var i=0; i < arr.length; i++){ // for-nya dibatesin jadi kurang 2 indeks, karena kondisi if i+1 i+2 
     if(arr[i+1]-arr[i]===deret){
       arit+=1;
     }
